26b561e2740c3d3bb174ba06fa682d5953dd51eb
Gislaine S. Mendonça <gislainesmendonca@gmail.com>
2026-01-14 23:07:36 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-slate-900 h-screen text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-green-400">
+      </h1>
+    </div>
+  )
+}
+
+export default App
